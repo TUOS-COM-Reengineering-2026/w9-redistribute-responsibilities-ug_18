d7caa71eafcaec2195c7693f7823912f2db4612d
Anikesh Suresh <sureshanikesh@yahoo.co.uk>
2026-04-28 14:28:39 +0100

    Moved branch opening time field from bank to branch. Added get/set functions

--- Branch.py
+++ Branch.py
@@ -6,6 +6,7 @@ class Branch:
     def __init__(self, location):
         self._location = location
         self._staff = []
+        self._opening_time = ""
 
     def get_location(self):
         return self._location
@@ -15,3 +16,10 @@ class Branch:
 
     def get_staff(self):
         return self._staff
+    
+    def get_opening_time(self):
+        return self._opening_time
+    
+    def set_opening_time(self, opening_time):
+        self._opening_time = opening_time
+    
